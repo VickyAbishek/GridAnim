8b201f346cf0dff692237f878fe5604efec417a0
VickyAbishek <vicky.abishek@outlook.com>
2016-10-09 19:10:06 +0530

    Completed Add on Click

--- js/application.js
+++ js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
-	$('#b1').on('click' function () {
-		var nq = '<div class="square"></div>';
- 		//$('.main-div div').append(nq);	
- 		$(this).closest('.main-div').find('.square').addClass('col');
- 			});
+	$('button').on('click', function () {
+		
+		/*var x = '<p>hel</p>';
+		$(this).css('color','white');
+		var nq = '<div class="square col"></div>';
+ 		$(this).removeClass('b1');
+ 		var sq = $(this).next().find('.square');
+ 		sq.addClass('col');	*/
+ 		
+ 		//$(this).next().after(x);
+ 		//$(this).next().next().append(nq);
+
+ 		var squarecontainer = 	$(this).next().next();
+ 		var addsquare = '<div class="square col"></div>';
+ 		squarecontainer.append(addsquare);
+ 		squarecontainer.find('.square').addClass('col');
+ 	});
 });
